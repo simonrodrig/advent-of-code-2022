531ec5b5f9bf464d890ad2514c420b2f610f0e43
Simon Rodriguez <esimonrodrig@gmail.com>
2022-12-01 12:17:23 -0500

    part02 complete

---
+++ day01/part02.ts
@@ -0,0 +1,30 @@
+import { rl, sum, numericSort } from '../utils';
+
+async function main() {
+  const elves: number[][] = [];
+  let elf: number[] = [];
+
+  for await (let line of rl) {
+    line = line.trim();
+
+    if (line !== '') {
+      elf.push(+line);
+    } else {
+      console.log('elf', elf);
+      elves.push(elf);
+      elf = [];
+    }
+  }
+
+  if (elf.length > 0) elves.push(elf);
+
+  const elfSums = numericSort(elves.map((e) => sum(e)));
+
+  console.log('all elves:', elfSums);
+
+  console.log('sum of highest three: ', sum(elfSums.slice(0, 3)));
+}
+
+if (require.main === module) {
+  main();
+}
--- utils/index.ts
+++ utils/index.ts
@@ -3,6 +3,8 @@ import readline from 'readline';
 export const sum = (nums: number[]) =>
   nums.reduce((acc, curr) => acc + curr, 0);
 
+export const numericSort = (nums: number[]) => nums.sort((a, b) => b - a);
+
 export const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout,
