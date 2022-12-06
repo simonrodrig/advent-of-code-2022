56511a25ace73338c948c210012a03e3a03a0e8b
Simon Rodriguez <esimonrodrig@gmail.com>
2022-12-06 15:52:23 -0500

    day06: complete

---
+++ day06/main.ts
@@ -0,0 +1,47 @@
+#!/usr/bin/env -S deno run
+import { readLines } from 'std/io/mod.ts';
+import * as flags from 'std/flags/mod.ts';
+import { bgGreen, bgRed, red } from 'std/fmt/colors.ts';
+
+async function main(numUniqueChars: number) {
+  for await (const line of readLines(Deno.stdin)) {
+    const seen = new Set<string>();
+
+    for (const [idx, char] of [...line].entries()) {
+      /**
+       * I'm abusing the fact that JS sets generally have ordered insertion.
+       * probably not the best idea in the world though. If we were using a queue
+       * instead, i'd probably just pop front a bunch.
+       */
+      while (seen.has(char)) {
+        seen.delete([...seen][0]);
+      }
+      seen.add(char);
+
+      // We found our slice!
+      if (seen.size == numUniqueChars) {
+        console.log(
+          `${
+            line.slice(0, idx - numUniqueChars) +
+            bgGreen(line.slice(idx + 1 - numUniqueChars, idx + 1)) +
+            line.slice(idx, line.length)
+          } | Marker found after ${idx + 1} chars`,
+        );
+        break;
+      }
+    }
+  }
+}
+
+
+if (import.meta.main) {
+  const f = flags.parse(Deno.args);
+  if (f.part == 1) {
+    main(4);
+  } else if (f.part == 2) {
+    main(14);
+  } else {
+    console.error(bgRed('ERROR'), red('No part specified'));
+    Deno.exit(1);
+  }
+}
