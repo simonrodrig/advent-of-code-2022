4e514d9fe6944fc95ed64d4ed2c011e8909551cd
Simon Rodriguez <esimonrodrig@gmail.com>
2022-12-05 22:57:34 -0500

    deno stay winning

--- README.md
+++ README.md
@@ -1,6 +1,16 @@
 # advent-of-code-2022
 My solutions for [Advent of Code 2022](https://adventofcode.com/2022).
 
+## Deno
+This branch is for my solutions written for (Deno)[https://deno.land], which is
+a JavaScript/TypeScript alternative engine to Node.js. The main benefits of Deno
+over something like Node is a comprehensive Standard Library, built-in
+formatter, linter, task runner, and--most importantly--first-class support for
+TypeScript, which is something you have to get around in Node by using something
+like `ts-node`. Every script should include a `shebang` so you can run each of
+the problems like any other script. Be sure to specify `--part=1` or `--part=2`
+depending on which part you wish to solve.
+
 ## Branches
 The [main](https://github.com/simonrodrig/advent-of-code-2022/tree/main) branch will most likely be TypeScript solutions. If I try out the challenges this year
 in some new languages, they will be split across different branches.
---
+++ day01/main.ts
@@ -0,0 +1,60 @@
+#!/usr/bin/env -S deno run
+import { readLines } from 'std/io/mod.ts';
+import * as flags from 'std/flags/mod.ts';
+
+async function part01() {
+  let highestCal = 0;
+  let currCal = 0;
+  for await (let line of readLines(Deno.stdin)) {
+    line = line.trim();
+
+    if (line.length == 0) {
+      highestCal = Math.max(highestCal, currCal);
+      currCal = 0;
+    } else {
+      currCal += +line;
+    }
+  }
+
+  if (currCal != 0) {
+    highestCal = Math.max(highestCal, currCal);
+  }
+
+  console.log('Highest Cal Elf:', highestCal);
+}
+
+async function part02() {
+  const highestCalElves = [0, 0, 0];
+  let currCal = 0;
+
+  const updateThreeHighest = (cal: number) => {
+    const idx = highestCalElves.findIndex(c => c === Math.min(...highestCalElves));
+    highestCalElves[idx] = Math.max(highestCalElves[idx], cal);
+  }
+
+  for await (let line of readLines(Deno.stdin)) {
+    line = line.trim();
+
+    if (line.length == 0) {
+      updateThreeHighest(currCal)
+      currCal = 0;
+    } else {
+      currCal += +line;
+    }
+  }
+
+  if (currCal != 0) {
+    updateThreeHighest(currCal)
+  }
+
+  console.log('Highest Three Elves:', highestCalElves.reduce((p, c) => p + c));
+}
+
+if (import.meta.main) {
+  const f = flags.parse(Deno.args);
+  if (f.part == 1) {
+    part01();
+  } else if (f.part == 2) {
+    part02();
+  }
+}
