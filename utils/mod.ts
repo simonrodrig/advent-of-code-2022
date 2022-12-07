bf79c0b35b5cc0094935aaf08e1bd7cdae42689f
Simon Rodriguez <esimonrodrig@gmail.com>
2022-12-07 11:42:13 -0500

    port DefaultDict to TS

--- utils/mod.ts
+++ utils/mod.ts
@@ -39,3 +39,30 @@ export const coloredSlice = (
   str.slice(0, startIdx) +
   color(str.slice(startIdx, endIdx)) +
   str.slice(endIdx, str.length);
+
+/**
+ * Port of Python's DefaultDict. Ends up being useful in a lot of situations
+ * involving trees.
+ */
+export class DefaultDict<T1, T2> {
+  private constructorFn: () => T2;
+  private internalMap = new Map<T1, T2>();
+
+  constructor(constructorFn: () => T2) {
+    this.constructorFn = constructorFn;
+  }
+
+  get(key: T1): T2 {
+    const possibleVal = this.internalMap.get(key);
+
+    if (possibleVal) {
+      return possibleVal;
+    } else {
+      return this.constructorFn();
+    }
+  }
+
+  set(key: T1, val: T2) {
+    this.internalMap.set(key, val);
+  }
+}
