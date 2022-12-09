0eb1de392775447e703008c412c395b1a03a99ad
Simon Rodriguez <esimonrodrig@gmail.com>
2022-12-08 23:46:34 -0500

    day08: done

--- day08/main.ts
+++ day08/main.ts
@@ -38,21 +38,81 @@ function countVisibleTrees(trees: number[][]) {
   return count;
 }
 
-async function main() {
+function computeTreesThatCanBeSeen(treeRow: number[], currTree: number) {
+  let numTreesThatCanBeSeen = 0;
+
+  for (const tree of treeRow) {
+    numTreesThatCanBeSeen++;
+
+    if (tree >= currTree) {
+      break;
+    }
+  }
+
+  return numTreesThatCanBeSeen;
+}
+
+function computeHighestScenicScore(trees: number[][]): number {
+  let highestScore = 0;
+
+  for (let i = 0; i < trees.length; i++) {
+    const currRow = trees[i];
+    for (let j = 0; j < currRow.length; j++) {
+      // Check Left
+      const leftSlice = currRow.slice(0, j);
+      const leftScore = computeTreesThatCanBeSeen(
+        leftSlice.reverse(),
+        trees[i][j],
+      );
+
+      // Check Right
+      const rightSlice = currRow.slice(j + 1, currRow.length);
+      const rightScore = computeTreesThatCanBeSeen(rightSlice, trees[i][j]);
+
+      const currCol = get2DArrayColumn(trees, j);
+
+      // Check Above
+      const topSlice = currCol.slice(0, i);
+      const topScore = computeTreesThatCanBeSeen(
+        topSlice.reverse(),
+        trees[i][j],
+      );
+
+      // Check Below
+      const bottomSlice = currCol.slice(i + 1, currCol.length);
+      const bottomScore = computeTreesThatCanBeSeen(bottomSlice, trees[i][j]);
+
+      // Update the highest
+      highestScore = Math.max(
+        highestScore,
+        leftScore * rightScore * topScore * bottomScore,
+      );
+    }
+  }
+
+  return highestScore;
+}
+
+async function main(part: 1 | 2) {
   const trees: number[][] = [];
   for await (const line of readLines(Deno.stdin)) {
     trees.push(line.split('').map((tree) => +tree));
   }
 
-  console.log('Visible Trees:', countVisibleTrees(trees));
+  if (part == 1) {
+    console.log('Visible Trees:', countVisibleTrees(trees));
+  } else {
+    const highestScore = computeHighestScenicScore(trees);
+    console.log('Highest Scenic Score:', highestScore);
+  }
 }
 
 if (import.meta.main) {
   const f = flags.parse(Deno.args);
   if (f.part == 1) {
-    main();
+    main(1);
   } else if (f.part == 2) {
-    main();
+    main(2);
   } else {
     console.error(bgRed('ERROR'), red('No part specified'));
     Deno.exit(1);
